6ebd1265b8d95ba44925e5fdeb6e67911e27f002
Павел Шлыков <shlicha@yandex.ru>
2024-08-26 21:36:01 +0300

    Домашняя работа 9.2

---
+++ module_9_2.py
@@ -0,0 +1,9 @@
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+first_result = [len(fs) for fs in first_strings if len(fs) > 4]
+second_result = [(fs,ss) for fs in first_strings for ss in second_strings if len(fs) == len(ss)]
+third_result = {st: len(st) for st in (first_strings + second_strings) if len(st) % 2 == 0}
+print(first_result)
+print(second_result)
+print(third_result)
