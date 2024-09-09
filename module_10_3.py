2b8b82c113ef504004a8fc44738feb27f3925eb9
Павел Шлыков <shlicha@yandex.ru>
2024-09-09 22:31:47 +0300

    Домашняя работа 10.3

---
+++ module_10_3.py
@@ -0,0 +1,43 @@
+from random import randint
+from threading import  Thread, Lock
+from time import sleep
+
+
+class Bank:
+    balance: int = 0
+    lock = Lock()
+
+    def __init__(self):
+        pass
+
+    def deposit(self):
+        for i in range(100):
+            entrance = randint(50, 500)
+            self.balance += entrance
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
+            print(f'Пополнение: {entrance}. Баланс: {self.balance}')
+            sleep(0.001)
+
+    def take(self):
+        for i in range(100):
+            cut = randint(50, 500)
+            print(f'Запрос на {cut}')
+            if cut <= self.balance:
+                self.balance -= cut
+                print(f'Снятие: {cut}, Баланс: {self.balance}')
+            else:
+                print('Запрос отклонен. Недостаточно средств')
+                self.lock.acquire()
+            sleep(0.001)
+
+
+bk = Bank()
+th1 = Thread(target=Bank.deposit, args=(bk,))
+th2 = Thread(target=Bank.take, args=(bk,))
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+
+print(f'Итоговый баланс: {bk.balance}')
