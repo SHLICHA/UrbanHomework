e12e2e96378f941921ba934a19dae4466a594a44
Павел Шлыков <shlicha@yandex.ru>
2024-10-31 22:50:48 +0300

    Домашняя работа 14.4

---
+++ crud_functions.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+
+def initiate_db():
+    connection = sqlite3.connect('not_telegram.db')
+    cursor = connection.cursor()
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS Products (
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        description TEXT,
+        price INTEGER NOT NULL
+        ) 
+        '''
+    )
+    connection.commit()
+    connection.close()
+
+
+def get_all_products():
+    connection = sqlite3.connect('not_telegram.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM Products')
+    products = cursor.fetchall()
+    connection.commit()
+    connection.close()
+    return products
+
+
+
+
+
