19932721b7822421dbfa04c95b858a1bd21e35f9
Павел Шлыков <shlicha@yandex.ru>
2024-11-19 21:29:55 +0300

    Домашняя работа 16.1

---
+++ FastAPI/module_16_1.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+async def main() -> str:
+    return 'Главная страница'
+
+@app.get('/user/admin')
+async def admin() -> str:
+    return 'Вы вошли как администратор'
+
+@app.get('/user/{user_id}')
+async def user(user_id) -> str:
+    return f'Вы вошли как пользователь №{user_id}'
+
+@app.get('/user')
+async def user(username: str = 'Pavel', age: int = 33) -> str:
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
+
