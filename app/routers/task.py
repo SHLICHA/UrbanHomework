2ed00f5d1eebceec78ecfecec11ab062b6a45938
Павел Шлыков <shlicha@yandex.ru>
2024-11-26 17:45:38 +0300

    Модуль 17.2

--- app/routers/task.py
+++ app/routers/task.py
@@ -1,20 +1,4 @@
 from fastapi import APIRouter
-from sqlalchemy import Boolean, ForeignKey, Column, Integer, String
-from sqlalchemy.orm import relationship
-
-from app.backend.db import Base
-
-
-class Task(Base):
-    __tablename__ = 'tasks'
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    content = Column(String)
-    priority = Column(Integer, default=0)
-    completed = Column(Boolean, default=False)
-    user_id = Column(Integer, ForeignKey('users'), nullable=True)
-    slug = Column(String, unique=True, index=True)
-    user = relationship("User", back_populates='tasks')
 
 
 router = APIRouter(prefix='/task', tags=['task'])
