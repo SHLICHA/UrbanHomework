6455d9ed201dfd95b900d45cfab49e01c7c99bfc
Павел Шлыков <shlicha@yandex.ru>
2024-11-26 17:44:40 +0300

    Модуль 17.2

--- app/models/task.py
+++ app/models/task.py
@@ -1,45 +1,22 @@
-from fastapi import APIRouter
 from sqlalchemy import Boolean, ForeignKey, Column, Integer, String
+from sqlalchemy.schema import CreateTable
 from sqlalchemy.orm import relationship
 
 from app.backend.db import Base
+from app.models import *
 
 
 class Task(Base):
     __tablename__ = 'tasks'
+    __table_args__ = ({'extend_existing': True},)
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     content = Column(String)
     priority = Column(Integer, default=0)
     completed = Column(Boolean, default=False)
-    user_id = Column(Integer, ForeignKey('users'), nullable=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=True)
     slug = Column(String, unique=True, index=True)
     user = relationship("User", back_populates='tasks')
 
 
-router = APIRouter(prefix='/task', tags=['task'])
-
-
-@router.get('/')
-async def all_tasks():
-    pass
-
-
-@router.get('/task_id')
-async def task_by_id():
-    pass
-
-
-@router.post('/create')
-async def create_task():
-    pass
-
-
-@router.put('/update')
-async def update_task():
-    pass
-
-
-@router.delete('delete')
-async def delete_task():
-    pass
+print(CreateTable(Task.__table__))
